27420440cf6d9d9b7fd65ab992e680633a13950a
Chris Frank <chris@thenepotist.com>
2015-06-23 21:52:37 -0400

    max db connections set to ENV variable

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-SITE="http://tfp-redirects-production.herokuapp.com/"
+SITE="http://tfp-redirects-production.herokuapp.com/redirects"
 TRIALS=1000
 CONCURRENCY=100
 ab -n $TRIALS -c $CONCURRENCY $SITE
