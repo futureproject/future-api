d5a2fb571086aa36c9ffbb4137847a95662ca173
Chris Frank <chris@thenepotist.com>
2015-06-25 00:09:56 -0400

    smarter defaults for db

--- db.rb
+++ db.rb
@@ -1,13 +1,18 @@
 ENV['RACK_ENV'] ||= 'development'
 
 class Frank < Sinatra::Base
-  configure do
+  def self.set_database
     case ENV["RACK_ENV"]
     when 'development'
       set :database, Sequel.postgres(database: 'tfp_redirects_development', host: 'localhost')
     else
-      set :database, Sequel.connect(ENV['DATABASE_URL'], max_connections: ENV['MAX_DB_CONNECTIONS'] || 20, single_threaded: false)
+      set :database, Sequel.connect(ENV['DATABASE_URL'], max_connections: ENV['MAX_DB_CONNECTIONS'] || 4, single_threaded: false)
     end
   end
+
+  configure do
+    self.set_database
+  end
+
 end
 
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-SITE="http://tfp-redirects-production.herokuapp.com/redirects"
+SITE="http://go.dream.org/apply"
 TRIALS=1000
-CONCURRENCY=100
+CONCURRENCY=50
 ab -n $TRIALS -c $CONCURRENCY $SITE
