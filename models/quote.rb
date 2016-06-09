59fb87f4de48c4fe2956c756ece744366786f7fd
Chris Frank <chris.frank@thefutureproject.org>
2016-06-09 15:20:48 +0000

    added YAML quotes

--- models/quote.rb
+++ models/quote.rb
@@ -1,17 +1,27 @@
 class Quote < Sequel::Model
   plugin :validation_helpers
 
+  #returns a hard-coded default quote
   def self.default
-    self.new(body: "Education is what is left after one has forgotten everything one learned in school.", source: "Einstein")
+    self.new(
+      body: "Education is what is left after one has forgotten everything one learned in school.",
+      source: "Albert Einstein"
+    )
   end
 
-  def self.daily
-    q = self.offset(rand(self.all.count.to_i)).first
-    q || self.default
+  #cycles through the quotes in the DB, returning one each day
+  def self.daily(manual_offset = 0)
+    count = self.count
+    if count > 0
+      self.offset((Date.today.yday + manual_offset) % count)
+    else
+      self.default
+    end
   end
 
   def validate
     super
     validates_presence [:body, :source]
   end
+
 end
