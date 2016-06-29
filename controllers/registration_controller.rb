291cfd5f6a4af8c8209f1c4760fd54937d93458c
Chris Frank <chris.frank@thefutureproject.org>
2016-06-29 17:48:44 -0400

    Slack prompt

--- controllers/registration_controller.rb
+++ controllers/registration_controller.rb
@@ -1,8 +1,9 @@
 class RegistrationController < ApplicationController
 
   get "/" do
+    #block people who haven't had a registration token set by Oauth
+    halt(406) unless session[:registration_token]
     @email = session[:registration_token]
-    halt(406) unless @email
     @slack_user = SlackUser.find_by_email(@email)
     if @slack_user
       @employee = Employee.find_or_create_by_slack_id(@slack_user.id)
@@ -22,6 +23,11 @@ class RegistrationController < ApplicationController
     else
       "It didn't work"
     end
+  end
+
+  get "/join_slack" do
+    @email = "dreamo@thefutureproject.org"
+    erb :"registration/join_slack_first"
   end
 
   private
