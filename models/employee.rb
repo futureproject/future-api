db4b1d12a14590447686cc12c7852b3a90e413f8
Chris Frank <chris.frank@thefutureproject.org>
2016-06-29 16:41:29 -0400

    Employee.update uses a slower but safer API call, so as not to overwrite blank data with nils

--- models/employee.rb
+++ models/employee.rb
@@ -12,7 +12,7 @@ class Employee
 
   def self.update(attrs)
     id = attrs[:id] || attrs["id"]
-    record = Airtable::Record.new("id" => id)
+    record = table.find(id)
     attrs.each { |k,v| record[k] = v }
     if self.table.update(record)
       record
