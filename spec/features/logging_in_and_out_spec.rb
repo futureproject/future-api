f141860ffc3bce49dc830a50f488871bd0cc763b
Chris Frank <chris.frank@thefutureproject.org>
2016-07-15 18:39:33 -0400

    Restored auth test for malicious users

--- spec/features/logging_in_and_out_spec.rb
+++ spec/features/logging_in_and_out_spec.rb
@@ -10,4 +10,9 @@ feature "Logging" do
     click_link "Log Out"
     expect(page).to have_content "logged out"
   end
+  scenario "in as an unauthorized user" do
+    disable_automatic_auth
+    visit "/"
+    expect(page).to have_content "Auth error"
+  end
 end
