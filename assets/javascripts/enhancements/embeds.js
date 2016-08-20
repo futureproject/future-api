9e75492e3df6a863a178ac8276621fe84d4803a1
Chris Frank <chris.frank@thefutureproject.org>
2016-08-20 14:05:08 -0400

    embeds now show a loading spinner when they are images

--- assets/javascripts/enhancements/embeds.js
+++ assets/javascripts/enhancements/embeds.js
@@ -5,9 +5,24 @@ tfp.Embed = function(elem) {
   $elem.one("click", ".module-header", function(){
     var url = $(this).find("a").attr("href");
     var extension = url.split("/").pop();
-    var embed = extension.match(/jpg|jpeg|png|gif/i) ? document.createElement("img") : document.createElement("iframe");
-    embed.src = url;
-    $elem.find(".module-content").prepend(embed);
+    if (extension.match(/jpg|jpeg|png|gif/i)) {
+      var embed = document.createElement("img");
+      var $target = $(elem).find(".module-content");
+      var $loader = $("<div class='loading' />");
+      $target.prepend($loader);
+      embed.src = url;
+      $(embed).one('load', function(){
+        $loader.replaceWith(embed);
+      }).each(function(){
+        if (this.complete) {
+          $(this).trigger('load')
+        }
+      })
+    } else {
+      var embed = document.createElement("iframe");
+      embed.src = url;
+      $elem.find(".module-content").prepend(embed);
+    }
   })
 }
 
