1889803788bf0f2b0b5b3fded31ab0b2d319a9ff
Chris Frank <chris.frank@thefutureproject.org>
2016-09-06 15:26:52 -0400

    prepping for students

---
+++ models/student.rb
@@ -0,0 +1,10 @@
+class Student < Airtable::Model
+
+  # student is a WEIRD class, because we store each city's students
+  # in a separate airtable base. pretty much every accessor method is custom
+
+  def self.all
+
+  end
+
+end
