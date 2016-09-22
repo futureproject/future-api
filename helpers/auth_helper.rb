67dfa48dfeaf5965fdb59c87e6b4b9a85848e5c4
Chris Frank <chris.frank@thefutureproject.org>
2016-09-22 14:15:49 -0400

    Logging out now also clears registration token

--- helpers/auth_helper.rb
+++ helpers/auth_helper.rb
@@ -31,9 +31,8 @@ module AuthHelper
   end
 
   def sign_out user
-    App.cache.delete user.cache_key
-    session[:auth_token] = nil
-    session[:registration_token] = nil
+    App.cache.delete(user.cache_key) if user
+    session[:auth_token] = session[:registration_token] = nil
   end
 
   def get_email_from_auth_hash(hash=request.env["omniauth.auth"]["info"])
