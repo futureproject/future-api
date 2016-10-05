16402abe99e31215d204cafa6ec43ccdced7a6c0
Chris Frank <chris.frank@thefutureproject.org>
2016-10-05 12:48:27 -0400

    added Bold Move descriptions to dreamo

--- models/bold_move.rb
+++ models/bold_move.rb
@@ -4,31 +4,31 @@ class BoldMove
     [
       {
         title: "Science",
-        description: "lorem ipsum"
+        description: "Distill – and start to prove out & package – our science for unlocking the possibility of people."
       },
       {
         title: "Schools",
-        description: "dolor sit"
+        description: "Continuously improve – and polish – the TFP school program, making it better for all students and more likely to grow organically."
       },
       {
         title: "Digital",
-        description: "amet etc"
+        description: "Design the tool that makes it possible for anyone on earth to access our tools and coaching in the most accessible way we can imagine."
       },
       {
         title: "Community",
-        description: "lorem ipsum"
+        description: "Unite our students, schools, and supporters as one team with a common identity and purpose – and build the demand to grow our community exponentially."
       },
       {
         title: "Capital",
-        description: "dolor sit"
+        description: "Raise the funds we will need to fulfill our next two years and develop world- class resource engine for fulfilling our goals without compromise."
       },
       {
         title: "Team",
-        description: "amet etc"
+        description: "Build systems and culture to make society’s creative team possible – and ensure every teammate is perpetually practicing possibility."
       },
       {
         title: "Future",
-        description: "and many more"
+        description: "Launch our incubator for other projects, design our ‘17 national event, and design our strategy for changing the world."
       }
     ]
   end
