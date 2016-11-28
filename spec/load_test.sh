cd3cd491373f0d6a386541ea4f91071540b6017e
Chris Frank <chris.frank@thefutureproject.org>
2016-11-28 18:44:47 +0000

    thin prep

--- spec/load_test.sh
+++ spec/load_test.sh
@@ -1,5 +1,4 @@
-SITE="http://go.dream.org/widgets/profiles"
-SITE="http://localhost:5000/expensive_test"
+SITE="http://go.dream.org/less_expensive_test"
 TRIALS=1000
 CONCURRENCY=30
 ab -n $TRIALS -c $CONCURRENCY $SITE
