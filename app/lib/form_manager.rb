6c7f9d249da8b10d33a837fd39d299a3f682df9e
Chris Frank <chris.frank@thefutureproject.org>
2017-01-23 22:16:32 +0000

    FormManager.forms_for no longer throws an error for users with no city

--- app/lib/form_manager.rb
+++ app/lib/form_manager.rb
@@ -2,7 +2,7 @@ module FormManager
   @@forms = YAML.load_file("#{App.root}/config/db/forms.yml")
 
   def self.forms_for(user)
-    return nil if user.goddamn_city.match(/hq/i)
+    return nil if (user.goddamn_city && user.goddamn_city.match(/hq/i))
     forms = {}
     @@forms.each do |key, value|
       forms[key] = value[user.goddamn_city]
